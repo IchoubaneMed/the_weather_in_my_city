d12caa6cc55dae3990ab8e30a976ab2d7e308cd9
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-03 20:31:16 +0100

    Retrive the weather data from the openweather map api using fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Header from './components/Header';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
-
-  const [city, setCity] = useState('Toronto');
+  // Create the variables city, input, and data 
+  // as well as the setCity, setInput, and setData functions via useState hook
+  const [city, setCity] = useState("");
   const [input, setInput] = useState("");
+  const [data, setData] = useState("");
+
+  // Create the useEffect function
+  useEffect(() => {
+    // Retrieve the weather data from the openweather map api via the fetch api
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=efc7c87235c26a8f4739f0b5fdd3d678`)
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, [city]);
 
   function handleChange(event) {
     setInput(event.target.value)
@@ -19,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <Header onSubmit={handleSubmit} input={input} onChange={handleChange}/>
-      {city}
+      {JSON.stringify(data)}
     </div>
   );
 }
