dd14d5b6b0485c57ee49a5ef1d16a1cdfaf73af8
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-10 23:39:03 +0100

    Added icon and description to the InformationCard component

--- src/components/InformationCard.js
+++ src/components/InformationCard.js
@@ -5,10 +5,15 @@ import { FaWind } from 'react-icons/fa';
 import { MdWaterDrop } from 'react-icons/md';
 
 
-const InformationCard = ({ visibility, wind, humidity }) => {
+const InformationCard = ({ visibility, wind, humidity, icon, main }) => {
+    const weather_icon = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
     return (
         <div className="info_card">
             <div className="card_content">
+                <img src={weather_icon} alt="weather icon" className="weather_icon"/>
+                <h1>{main}</h1>
+                <hr />
                 <h1><MdVisibility /> visibility: {visibility}m</h1>
                 <hr />
                 <h1><FaWind /> wind speed: {wind}m/s</h1>
