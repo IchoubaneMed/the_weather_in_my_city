2111a254c0a2035bab6c40c94ff42d8295fddf4f
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-10 16:18:58 +0100

    Displayed additional information

--- src/components/InformationCard.js
+++ src/components/InformationCard.js
@@ -1,7 +1,23 @@
-const InformationCard = () => {
-    return(
-        <div>
+import "./InformationCard.css";
+import { HiInformationCircle } from 'react-icons/hi';
+import { MdVisibility } from 'react-icons/md';
+import { FaWind } from 'react-icons/fa';
+import { MdWaterDrop } from 'react-icons/md';
 
+
+const InformationCard = ({ visibility, wind, humidity }) => {
+    return (
+        <div className="info_card">
+            <div className="card_content">
+                <h1><MdVisibility /> visibility: {visibility}m</h1>
+                <hr />
+                <h1><FaWind /> wind speed: {wind}m/s</h1>
+                <hr />
+                <h1><MdWaterDrop /> humidity: {humidity}%</h1>
+            </div>
+            <HiInformationCircle className="info_icon" />
         </div>
     );
 }
+
+export default InformationCard;
